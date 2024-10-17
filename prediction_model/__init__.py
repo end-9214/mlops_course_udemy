8c76d86eff9fd85941e0abc3d44addfc9e9c567e
Karamveer Singh <karamveersingh2003111@Gmail.com>
2024-10-17 10:32:13 +0530

    updated the pipeline.py

--- prediction_model/__init__.py
+++ prediction_model/__init__.py
@@ -1,5 +1,4 @@
 import os
 from prediction_model.config import config
-with open(os.path.join(config.PACKAGE_ROOT, 'VERSION')) as F:
+with open(os.path.join(config.PACKAGE_ROOT, 'VERSION')) as f:
     __version__ = f.read().strip()
-    
